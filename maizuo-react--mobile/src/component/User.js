6959404f76187f7a39fc43fbce4f4b46555d692b
dllo <dllo@dllodeMac-mini-77.local>
2017-09-26 20:37:22 +0800

    给丹丹

--- maizuo-react--mobile/src/component/User.js
+++ maizuo-react--mobile/src/component/User.js
@@ -5,9 +5,19 @@ import React, {Component} from 'react'
 import '../assets/styles/user.css'
 class User extends Component {
     render (){
+        var userA = document.getElementsByClassName('user')
+        var passA = document.getElementsByClassName('pass')
+        var butA = document.getElementsByClassName('but')
+        
         return(
-            <div>
-                14
+            <div className="wrap">
+                <div className="box">
+                    <input type="text" className="user" placeholder="输入手机号/邮箱"/>
+                    <div className="input-bg" />
+                    <input type="text" className="pass" placeholder="输入密码/验证码"/>
+                    <div className="input-bg" />
+                    <button type="submit" className="but">登录</button>
+                </div>
             </div>
         )
     }
